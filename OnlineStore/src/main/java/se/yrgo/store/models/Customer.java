12af9a0a8c994d78e6ddba225c27d6d92a41dca0
anomalin <anomalins@gmail.com>
2024-12-11 10:17:10 +0100

    adding code in the customer class

--- OnlineStore/src/main/java/se/yrgo/store/models/Customer.java
+++ OnlineStore/src/main/java/se/yrgo/store/models/Customer.java
@@ -1,4 +1,59 @@
 package se.yrgo.store.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class contains the information needed to setup a customer,
+ * complete a purchase and view order history
+ * @author Malin Sundberg
+ */
 public class Customer {
+    private String customerID;
+    private String name;
+    private String email;
+    private List<Product> orderhistory;
+
+    public Customer(String customerID, String name, String email) {
+        this.customerID = customerID;
+        this.name = name;
+        if(email != null && email.contains("@")) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("Not a valid email address.");
+        }
+        this.orderhistory = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerID='" + customerID + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", orderhistory=" + orderhistory +
+                '}';
+    }
+
+    /**
+     * getOrderhistory allows the user to reach all of the previous purchases from a customer.
+     * @returns a List of products
+     */
+    public List<Product> getOrderhistory() {
+        return new ArrayList<>(orderhistory);
+    }
+
+    /**
+     * placeOrder allows the user to take a ShoppingCart and it's contents to
+     * finalize a purchase.
+     * @param cart
+     */
+    public void placeOrder(ShoppingCart cart) {
+        if (cart == null) {
+            throw new IllegalArgumentException("Your cart is empty.");
+        }
+        orderhistory.addAll(cart.getProducts());
+    }
+
+
 }
--- OnlineStore/src/test/java/se/yrgo/store/models/CustomerTest.java
+++ OnlineStore/src/test/java/se/yrgo/store/models/CustomerTest.java
@@ -1,4 +1,52 @@
 package se.yrgo.store.models;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomerTest {
+Customer testCustomer = new Customer("1", "Malin Sundberg", "malin@sundberg.se");
+Product testProduct = new Product("001", "Sweater", 500);
+Product testProduct2 = new Product("002", "Pants", 600);
+ShoppingCart testCart = new ShoppingCart();
+List<Product> testOrderhistory = new ArrayList<>();
+
+    @Test
+    public void placeOrderTest() {
+        testCart.addProduct(testProduct);
+        testCart.addProduct(testProduct2);
+        testCustomer.placeOrder(testCart);
+        List<Product> actual = testOrderhistory.addAll(testCart.getProducts());
+        List<Product> expected = new ArrayList<>(List.of(testProduct, testProduct2));
+        assertEquals(expected, actual);
+        assertEquals(2, actual.size());
+
+    }
+
+    @Test
+    public void getOrderhistoryTest() {
+        List<Product> expected = new ArrayList<>();
+        expected.add(testProduct);
+        expected.add(testProduct2);
+        expected.add(testProduct);
+        testCart.addProduct(testProduct);
+        testCart.addProduct(testProduct2);
+        testCustomer.placeOrder(testCart);
+        ShoppingCart testCart2 = new ShoppingCart();
+        testCart2.addProduct(testProduct);
+        testCustomer.placeOrder(testCart2);
+        List<Product> actual = testCustomer.getOrderhistory();
+        assertEquals(expected, actual);
+        assertIterableEquals(expected, actual);
+        assertEquals(3, actual.size());
+
+    }
+
+    @Test
+    public void customerWithInvalidEmailTest() {
+        assertThrows(IllegalArgumentException.class, () -> new Customer("1", "Malin", "malinsundberg.se"));
+    }
 }
