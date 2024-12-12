7f86ce0902cb56a05f3f496fcdf2d9fd0ad073e9
anomalin <anomalins@gmail.com>
2024-12-12 16:16:17 +0100

    adding javadoc to CustomerTest class

--- OnlineStore/src/test/java/se/yrgo/store/models/CustomerTest.java
+++ OnlineStore/src/test/java/se/yrgo/store/models/CustomerTest.java
@@ -7,6 +7,12 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The CustomerTest class contains three test methods to ensure that
+ * the user email address is valid, that the purchase has been successful and
+ * that you can view your order history.
+ *
+ */
 public class CustomerTest {
 Customer testCustomer = new Customer("Malin Sundberg", "malin@sundberg.se");
 Product testProduct = new Product(1, "Sweater", 500, 1);
@@ -14,6 +20,10 @@ Product testProduct2 = new Product(2, "Pants", 600, 1);
 ShoppingCart testCart = new ShoppingCart();
 List<Product> testOrderhistory = new ArrayList<>();
 
+    /**
+     * In the placeOrderTest the testOrderhistory list is tested with assertEquals to see
+     * that the expected value aligns with the actual value regarding to content and size.
+     */
     @Test
     public void placeOrderTest() {
         testCart.addProduct(testProduct);
@@ -26,6 +36,10 @@ List<Product> testOrderhistory = new ArrayList<>();
 
     }
 
+    /**
+     *  In the getOrderhistoryTest the testOrderhistory list is tested with assertEquals to see
+     *  that the expected value aligns with the actual value regarding to content and size.
+     */
     @Test
     public void getOrderhistoryTest() {
         List<Product> expected = new ArrayList<>();
@@ -45,6 +59,10 @@ List<Product> testOrderhistory = new ArrayList<>();
 
     }
 
+    /**
+     * customerWithInvalidEmailTest() checks that the program throws an IlleaalArugmentException
+     * if the email address is missing an @.
+     */
     @Test
     public void customerWithInvalidEmailTest() {
         assertThrows(IllegalArgumentException.class, () -> new Customer("Malin", "malinsundberg.se"));
